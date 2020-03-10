7b4d6a1ff0189cd6eba6a4e2b34810afa0536b38
Lucas <agtlucas@me.com>
2020-03-10 16:25:41 -0300

    Create basic HStack, VStack and Spacer

---
+++ index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const HStack = styled.View`
+  flex-direction: row;
+`;
+
+export const VStack = styled.View`
+  flex-direction: column;
+`;
+
+export const Spacer = styled.View`
+  flex: 1;
+`;
